fb9ff401c1b1a4e5cbadb2d375026ecf9573b3d9
lschendel <ls2484xmails@gmail.com>
2020-05-22 18:34:44 +0200

    Added a 3d scene to the window

--- CloneCraft/CloneCraft.cpp
+++ CloneCraft/CloneCraft.cpp
@@ -1,17 +1,48 @@
+#include <SFML/System.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
+#include "oldFunctions.h"
+
+void drawGame(sf::Vector2u wsize)
+{
+    glClearColor(0.3f, 0.3f, 0.3f, 1.f);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, wsize.x, wsize.y);
+    oldf::glu::Perspective(60, (float)wsize.x / (float)wsize.y, 0.1f, 512.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glPushMatrix();
+    glTranslatef(0.f, -2.f, -5.f);
+
+    glBegin(GL_QUADS);      // Draw The Cube Using quads
+
+    glColor3f(0.0f, 1.0f, 0.0f);  // Color Green
+    glNormal3f(0.f, 1.f, 0.f);
+    glVertex3f(1.0f, 1.0f, -1.0f);  // Top Right Of The Quad (Top)
+    glNormal3f(0.f, 1.f, 0.f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);  // Top Left Of The Quad (Top)
+    glNormal3f(0.f, 1.f, 0.f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);  // Bottom Left Of The Quad (Top)
+    glNormal3f(0.f, 1.f, 0.f);
+    glVertex3f(1.0f, 1.0f, 1.0f);  // Bottom Right Of The Quad (Top)
+    glEnd();
+}
 
 int main()
 {
     // create the window
-    sf::Window window(sf::VideoMode(800, 600), "Our Window", sf::Style::Default, sf::ContextSettings(32));
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Our Window", sf::Style::Default, sf::ContextSettings(32));
     window.setVerticalSyncEnabled(true);
 
     // activate the window
     window.setActive(true);
 
     // load resources, initialize the OpenGL states, ...
-
+    
     // run the main loop
     bool running = true;
     while (running)
@@ -37,7 +68,13 @@ int main()
 
         // draw...
 
-        // end the current frame (internally swaps the front and back buffers)
+        sf::Vector2u wsize = window.getSize();
+
+        drawGame(wsize);
+
+        glPopMatrix();
+
+        GLenum err = glGetError(); 
         window.display();
     }
 
