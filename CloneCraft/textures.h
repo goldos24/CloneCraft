1877138f2b2c6851e8fa293fdf19d6a5a4b29a4f
lschendel <ls2484xmails@gmail.com>
2020-05-23 19:25:14 +0200

    Added the first constant textures

--- CloneCraft/textures.h
+++ CloneCraft/textures.h
@@ -3,6 +3,7 @@
 
 namespace textures
 {
+
 	struct FaceTexture
 	{
 		FaceTexture(maths::Vec3 c1, maths::Vec3 c2, maths::Vec3 c3, maths::Vec3 c4)
@@ -40,5 +41,23 @@ namespace textures
 		FaceTexture* right;
 		FaceTexture* front;
 		FaceTexture* back;
-	};
+	}; 
+
+	namespace colors
+	{
+		const auto grey1 = maths::Vec3(0.4f, 0.4f, 0.4f);
+		const auto grey2 = maths::Vec3(0.35f, 0.35f, 0.35f);
+		const auto grey3 = maths::Vec3(0.3f, 0.3f, 0.3f);
+		const auto grey4 = maths::Vec3(0.25f, 0.25f, 0.25f);
+	}
+
+	namespace faceTextures
+	{
+		const FaceTexture* stone = new FaceTexture( colors::grey1, colors::grey3, colors::grey2, colors::grey4);
+	}
+
+	namespace blockTextures
+	{
+		const BlockTexture* stone = new BlockTexture(faceTextures::stone, faceTextures::stone, faceTextures::stone, faceTextures::stone, faceTextures::stone, faceTextures::stone);
+	}
 }
