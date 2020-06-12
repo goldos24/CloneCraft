5b8c9dbd0bac199ed20315323c89f82a86fab73b
Mister_JaJo <jarvistrigo@gmail.com>
2020-06-12 22:43:05 +0200

    Engoodened input

--- CloneCraft/input/input.h
+++ CloneCraft/input/input.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Mouse.hpp>
 #include <map>
 
 namespace input
@@ -8,8 +9,8 @@ namespace input
 
 	struct KeyPressState
 	{
-		KeyPressState() : pressed(false), elapsedTime(0) { }
-		KeyPressState(bool pressed) : pressed(pressed), elapsedTime(0) { }
+		KeyPressState();
+		KeyPressState(bool pressed);
 
 		float elapsedTime;
 		bool pressed;
@@ -17,109 +18,23 @@ namespace input
 
 	struct InputManager
 	{
-		InputManager()
-		{
-			for (int i = 0; i < sf::Mouse::Button::ButtonCount; i++) this->addMouseButton((sf::Mouse::Button)i);
-			for (int i = 0; i < sf::Keyboard::Key::KeyCount; i++) this->addKey((sf::Keyboard::Key)i);
-		}
+		InputManager();
 
-		void updateKeyPresses(float elapsedTime)
-		{
-			for (auto key : this->keyPressStates)
-			{
-				this->keyPressStates[key.first].pressed = sf::Keyboard::isKeyPressed(key.first);
-				
-				if (this->isKeyBeingPressed(key.first))
-				{
-					this->keyPressStates[key.first].elapsedTime += elapsedTime;
-				}
-				
-				if (this->isKeyPressed(key.first) || !this->isKeyBeingPressed(key.first))
-				{
-					this->keyPressStates[key.first].elapsedTime = 0;
-				}
-			}
-		}
-
-		void updateMouseButtonPresses()
-		{
-			for (auto b : this->mouseButtonPressStates)
-			{
-				this->mouseButtonPressStates[b.first] = sf::Mouse::isButtonPressed(b.first);
-			}
-		}
-
-		void resetAllTo(bool value)
-		{
-			for (auto a : this->keyPressStates)
-			{
-				this->keyPressStates[a.first] = value;
-			}
-
-			for (auto b : this->mouseButtonPressStates)
-			{
-				this->mouseButtonPressStates[b.first] = value;
-			}
-		}
-
-		void resetKeyTo(sf::Keyboard::Key key, bool value)
-		{
-			this->keyPressStates[key] = value;
-		}
-
-		void resetMouseButtonTo(sf::Mouse::Button button, bool value)
-		{
-			this->mouseButtonPressStates[button] = value;
-		}
-
-		void addMouseButton(sf::Mouse::Button button)
-		{
-			this->mouseButtonPressStates[button] = false;
-		}
-
-		void addKey(sf::Keyboard::Key key)
-		{
-			this->keyPressStates[key] = KeyPressState(false);
-		}
-
-		bool isKeyBeingPressed(sf::Keyboard::Key key)
-		{
-			return sf::Keyboard::isKeyPressed(key);
-		}
-
-		bool isKeyStillBeingPressedAfterDelay(sf::Keyboard::Key key, float delay)
-		{
-			if (this->keyPressStates[key].elapsedTime >= delay)
-			{
-				return this->isKeyBeingPressed(key);
-			}
-			return false;
-		}
-
-		bool isKeyReleased(sf::Keyboard::Key key)
-		{
-			return this->keyPressStates[key].pressed && !sf::Keyboard::isKeyPressed(key);
-		}
-
-		bool isKeyPressed(sf::Keyboard::Key key)
-		{
-			return !this->keyPressStates[key].pressed && sf::Keyboard::isKeyPressed(key);
-		}
-
-		bool isMouseButtonBeingPressed(sf::Mouse::Button button)
-		{
-			return sf::Mouse::isButtonPressed(button);
-		}
-
-		bool isMouseButtonReleased(sf::Mouse::Button button)
-		{
-			return this->mouseButtonPressStates[button] && !sf::Mouse::isButtonPressed(button);
-		}
-
-		bool isMouseButtonPressed(sf::Mouse::Button button)
-		{
-			return !this->mouseButtonPressStates[button] && sf::Mouse::isButtonPressed(button);
-		}
+		void updateKeyPresses(float elapsedTime);
+		void updateMouseButtonPresses();
+		void resetAllTo(bool pressed);
+		void resetKeyTo(sf::Keyboard::Key key, bool value);
+		void resetMouseButtonTo(sf::Mouse::Button button, bool value);
+		void addMouseButton(sf::Mouse::Button button);
+		void addKey(sf::Keyboard::Key key);
+		
+		bool isKeyBeingPressed(sf::Keyboard::Key key);
+		bool isKeyStillBeingPressedAfterDelay(sf::Keyboard::Key key, float delay);
+		bool isKeyReleased(sf::Keyboard::Key key);
+		bool isKeyPressed(sf::Keyboard::Key key);
+		bool isMouseButtonBeingPressed(sf::Mouse::Button button);
+		bool isMouseButtonReleased(sf::Mouse::Button button);
+		bool isMouseButtonPressed(sf::Mouse::Button button);
 
 		std::map<sf::Keyboard::Key, KeyPressState> keyPressStates;
 		std::map<sf::Mouse::Button, bool> mouseButtonPressStates;
