ba489621ec92353b1c634a42d98cb7269024d0b9
lschendel <ls2484xmails@gmail.com>
2020-06-13 14:09:55 +0200

    CPPified blockInfo

--- CloneCraft/world/blockInfo.h
+++ CloneCraft/world/blockInfo.h
@@ -9,52 +9,21 @@ namespace blox
 
 	struct Block
 	{
-		Block() {}
-		Block(ID id, std::string name, textures::BlockTexture* texture)
-		{
-			this->id = id;
-			this->name = name;
-			this->texture = texture;
-		}
+		Block();
+		Block(ID id, std::string name, textures::BlockTexture* texture);
 
 		textures::BlockTexture* texture;
 		ID id;
 		std::string name;
 	};
-	Block* InitBlocks()
-	{
-		auto* blocks = new Block[256];
-		blocks[air] = Block(air, "air", nullptr); //TODO nullptr
 
-		blocks[stone] = Block(stone, "stone", textures::blockTextures::stone);
-		blocks[grass] = Block(grass, "grass", textures::blockTextures::grass);
-		blocks[dirt] = Block(dirt, "dirt", textures::blockTextures::dirt);
+	Block* InitBlocks();
 
-		return blocks;
-	}
+	__declspec(selectany) Block* blocks = InitBlocks();
 
-	Block* blocks = InitBlocks();
+	Block& getByID(ID id);
 
-	Block& getByID(ID id)
-	{
-		return blocks[id];
-	}
-
-	Block& getByName(std::string name)
-	{
-		for (int i = 0; i < enumSize; i++)
-		{
-			if (blocks[i].name == name)
-			{
-				return blocks[i];
-			}
-		}
-		std::cout << "Couldn't find block with ID: " << name << std::endl;
-		return blocks[air];
-	}
+	Block& getByName(std::string name);
 }
 
-std::ostream& operator << (std::ostream& output, blox::ID id)
-{
-	return output << blox::getByID(id).name;
-}
+std::ostream& operator << (std::ostream& output, blox::ID id);
